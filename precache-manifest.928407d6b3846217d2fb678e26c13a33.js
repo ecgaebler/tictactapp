b60dbf50269eaad6156894fdf5aedc713c1ef648
ecgaebler <evan.gaebler@gmail.com>
2020-08-09 19:44:54 -0700

    Updates

---
+++ precache-manifest.928407d6b3846217d2fb678e26c13a33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f295eb5589695fa3c9b6b4029c22b890",
+    "url": "/ecgaebler/tictactapp/index.html"
+  },
+  {
+    "revision": "8dd1895c540205bfa717",
+    "url": "/ecgaebler/tictactapp/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "34cbee97d6fa54879dca",
+    "url": "/ecgaebler/tictactapp/static/js/2.42b57b7c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecgaebler/tictactapp/static/js/2.42b57b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dd1895c540205bfa717",
+    "url": "/ecgaebler/tictactapp/static/js/main.6c7e55f2.chunk.js"
+  },
+  {
+    "revision": "224773e9ff4f6622dc14",
+    "url": "/ecgaebler/tictactapp/static/js/runtime-main.b831a09b.js"
+  }
+]);
